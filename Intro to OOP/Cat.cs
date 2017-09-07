f81cb21b5fd35ec64cdcbb5ba6f2e65a6d04d585
karenmcewen <karenf_email@yahoo.com>
2017-09-06 20:50:31 -0400

    added class Dog

--- Intro to OOP/Cat.cs
+++ Intro to OOP/Cat.cs
@@ -8,16 +8,20 @@ namespace Intro_to_OOP
 {
     class Cat //new class - class names are singular nouns, PascalCase
     {
-        //creating fields for class Cat
+        //creating FIELDS for class Cat
         //private means only this class itself has access to it - uses camelCase
         private string name; 
         private string furColor;
+     
         private int furLength;
         private int age;
+
         private bool isMale;
         private bool isHungry;
+        private bool isHappy;
+        private bool isTired;
 
-        //public properties are needed to access private fields - they have the same name -uses PascalCase
+        //public PROPERTIES are needed to access private fields - they have the same name -uses PascalCase
         public string Name
         {
             get { return this.name; } //get the value and return it
@@ -30,11 +34,8 @@ namespace Intro_to_OOP
             set { this.furColor = "black"; }
         } 
 
-        public int FurLength
-        {
-            get { return this.furLength; }
-            set { this.furLength = 1; }
-        }
+        public int FurLength { get; set; } //another way to do this if you aren't putting in special values
+        
 
         public int Age
         {
@@ -42,6 +43,20 @@ namespace Intro_to_OOP
             
         }
 
+        public bool IsHappy
+        {
+            get { return this.isHappy; }
+            set { this.isHappy = false; }
+
+        }
+
+        public bool IsTired
+        {
+            get { return this.isHungry; }
+            set { this.isTired = false; }
+        }
+        
+        //CONSTRUCTOR
         //set a default constructor - has the same name as our Class() and no parameters - all classes need a default
        public Cat()
         {
@@ -63,7 +78,7 @@ namespace Intro_to_OOP
             this.age = age;
         }
 
-        public Cat(string name, string furColor, int furLength, int age, bool isMale, bool isHungry)
+        public Cat(string name, string furColor, int furLength, int age, bool isMale, bool isHungry, bool isTired)
             //these are the values that are being passed in
         {
             //these are instance variables that belong to the class
@@ -74,12 +89,57 @@ namespace Intro_to_OOP
             this.age = age;
             this.isMale = isMale;
             this.isHungry = isHungry;
-
+            this.isTired = isTired;
+           
         } 
 
+        //Creating behavior
+        public void Eat() //doesn't have the static
+        {
+            if(isHungry) //if the cat is hungry
+            {
+                this.isHungry = false; //"this" command references the instance of the object 
+                //once you feed it, no longer hungry!
+                Console.WriteLine(this.name + ": I'm stuffed...");
+                this.isHappy = true;
+            }
+            else if (!isHungry) //if the cat is not (! bang symbol) hungry
+            {
+                Console.WriteLine(this.name +  ": I'm good - no food for me.");
+            }
+        }
         
+        public void Run()
+        {
+            Console.WriteLine(this.name + ": I am running really fast!!");
+            this.isHungry = true;
+            Console.WriteLine(this.name + ": Now I need to eat!");
+        }
 
+        public void Purr()
+        {
+            if (!isHungry)
+            {
+                if (isHappy)
+                {
+                    Console.WriteLine("{0}: Purrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr", this.name);//another way to do this
+                    isTired = true;
+                }
+            }
+        }
 
-
+        public void Sleep()
+        {
+            if (!isHungry)
+            {
+                if (isHappy)
+                {
+                    if(isTired)
+                    {
+                        Console.WriteLine(this.name + ": Go away. I'm sleeping now.");
+                    }
+                }
+            }
+        }
     }
 }
--- Intro to OOP/Program.cs
+++ Intro to OOP/Program.cs
@@ -11,14 +11,28 @@ namespace Intro_to_OOP
         static void Main(string[] args)
         {
             Cat cat1 = new Cat();
-            Cat cat2 = new Cat("neko", "black", 3, 1, true, true);
+            Cat cat2 = new Cat("neko", "black", 3, 1, true, false, false);
 
-            Console.WriteLine(cat1.Name);
-            Console.WriteLine(cat2.Name);
-            cat1.Name = "gato";
-            Console.WriteLine(cat1.Name);
-           // cat1.furColor = "red"; //this is blocked because within the definition this is private
-           // Console.WriteLine(cat1.furColor); //this was never set so gives an error
+            //Console.WriteLine(cat1.Name);
+            //Console.WriteLine(cat2.Name);
+            //cat1.Name = "gato";
+            //Console.WriteLine(cat1.Name);
+            // cat1.furColor = "red"; //this is blocked because within the definition this is private
+            // Console.WriteLine(cat1.furColor); //this was never set so gives an error
+            cat2.Eat();
+            cat2.Run();
+            cat2.Eat();
+            cat2.Purr();
+            cat2.Sleep();
+            cat1.Run();
+            cat1.Eat();
+            cat1.Purr();
+            cat1.Sleep();
+
+            Dog dog1 = new Dog();
+
+
+
 
         }
     }
